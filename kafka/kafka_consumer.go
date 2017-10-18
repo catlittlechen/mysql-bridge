74c459e39d7c5e892b86e28ed0bcd9a5e10a0ca5
catchen <catlittlechen@gmail.com>
2017-10-18 08:11:06 +0800

    抽离ringbuffer,简化逻辑

--- kafka/kafka_consumer.go
+++ kafka/kafka_consumer.go
@@ -2,7 +2,6 @@ package kafka
 
 import (
 	"encoding/json"
-	"time"
 
 	"git.umlife.net/backend/mysql-bridge/global"
 	"github.com/Shopify/sarama"
@@ -29,8 +28,7 @@ type KafkaConsumer struct {
 
 	partitonsList []int32
 
-	ring []*ConsumerMessage
-	now  int
+	ring *RingBuffer
 
 	partitionConsumerArray []*PartitionMessage
 	channel                chan *ConsumerMessage
@@ -63,17 +61,17 @@ func NewKafkaConsumer(config KafkaConsumerConfig) (*KafkaConsumer, error) {
 	}
 	log.Infof("kafka client get partitions list %+v success", client.partitonsList)
 
-	client.ring = make([]*ConsumerMessage, config.RingLen)
-	client.now = 1
-	if client.offsetInfo.SequenceID == 0 {
-		client.batter = (config.DefaultSeqID + uint64(config.RingLen)) % global.MaxSeqID
-	} else {
-		client.batter = (client.offsetInfo.SequenceID + uint64(config.RingLen)) % global.MaxSeqID
+	seqID := config.DefaultSeqID
+	if seqID == 0 {
+		seqID = client.offsetInfo.SequenceID + 1
+		if seqID > global.MaxSeqID {
+			seqID = global.MinSeqID
+		}
 	}
+	client.ring = NewRingBuffer(config.RingLen, seqID, config.TimeSleep)
+	client.channel = client.ring.channel
 
 	client.partitionConsumerArray = make([]*PartitionMessage, len(client.partitonsList))
-	client.channel = make(chan *ConsumerMessage, 1024)
-
 	for index, pid := range client.partitonsList {
 		offset, ok := client.offsetInfo.PartitionOffset[pid]
 		if ok {
@@ -87,43 +85,6 @@ func NewKafkaConsumer(config KafkaConsumerConfig) (*KafkaConsumer, error) {
 		log.Infof("kafka client get %d partitions success", pid)
 	}
 	log.Infof("kafka client get all partitions success")
-
-	go func() {
-		exceptSeqID := client.batter - uint64(config.RingLen) + 1
-		if client.batter < uint64(config.RingLen) {
-			exceptSeqID = client.batter + global.MaxSeqID - uint64(config.RingLen) + 1
-		}
-		for {
-			for {
-				if client.closed {
-					return
-				}
-				if client.ring[client.now] != nil {
-					if client.ring[client.now].BinLog.SeqID == exceptSeqID {
-						log.Infof("get data index:%d exceptSeqID:%d", client.now, exceptSeqID)
-						break
-					}
-					log.Errorf("ring bug! index:%d seqID:%d exceptSeqID:%d", client.now, client.ring[client.now].BinLog.SeqID, exceptSeqID)
-				}
-				log.Infof("waiting for data... index:%d exceptSeqID:%d", client.now, exceptSeqID)
-				time.Sleep(client.cfg.TimeSleep)
-				continue
-			}
-			client.channel <- client.ring[client.now]
-			client.ring[client.now] = nil
-
-			client.now++
-			if client.now == client.cfg.RingLen {
-				client.now = 0
-				client.batter = (client.batter + uint64(config.RingLen)) % global.MaxSeqID
-			}
-
-			exceptSeqID++
-			if exceptSeqID > global.MaxSeqID {
-				exceptSeqID = global.MinSeqID
-			}
-		}
-	}()
 
 	return client, nil
 }
@@ -149,75 +110,7 @@ func (k *KafkaConsumer) NewPartitionMessgae(pid int32, offset int64) (*Partition
 				Msg:    msg,
 				BinLog: binlog,
 			}
-			for {
-				// deal with > maxSeqID
-				// if maxSeqID >> ringLen then that is right
-				// ignore when seqID in (now-9*ringLen, now)
-				log.Infof("batter %d now %d binlog:%d ringLen:%d min: %d max: %d", k.batter, k.now, binlog.SeqID, k.cfg.RingLen, global.MinSeqID, global.MaxSeqID)
-				if k.batter+uint64(k.now) > global.MaxSeqID {
-					if k.batter-uint64(k.cfg.RingLen-k.now) > binlog.SeqID && binlog.SeqID > k.batter-uint64(10*k.cfg.RingLen-k.now) {
-						break
-					}
-
-					if binlog.SeqID >= k.batter {
-						k.ring[binlog.SeqID-k.batter] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, binlog.SeqID-k.batter)
-						break
-					} else if binlog.SeqID < k.batter+uint64(k.now)-global.MaxSeqID {
-						k.ring[global.MaxSeqID-k.batter+binlog.SeqID] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, global.MaxSeqID-k.batter+binlog.SeqID)
-						break
-					} else if binlog.SeqID >= k.batter-uint64(k.cfg.RingLen-k.now+1) {
-						k.ring[k.cfg.RingLen-int(k.batter-binlog.SeqID)] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, k.cfg.RingLen-int(k.batter-binlog.SeqID))
-						break
-					} else {
-						time.Sleep(k.cfg.TimeSleep)
-					}
-
-					// batter 0 now 0 binlog:10000 ringLen:500 min: 1 max: 10000
-				} else if k.batter < uint64(k.cfg.RingLen-k.now+1) {
-					if k.batter+global.MaxSeqID-uint64(k.cfg.RingLen-k.now) > binlog.SeqID && binlog.SeqID > k.batter+global.MaxSeqID-uint64(10*k.cfg.RingLen-k.now) {
-						break
-					}
-
-					if k.batter > binlog.SeqID {
-						k.ring[k.cfg.RingLen-int(k.batter-binlog.SeqID)] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, k.cfg.RingLen-int(k.batter-binlog.SeqID))
-						break
-					} else if k.batter+uint64(k.now) > binlog.SeqID {
-						k.ring[binlog.SeqID-k.batter] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, binlog.SeqID-k.batter)
-						break
-					} else if k.batter+global.MaxSeqID-uint64(k.cfg.RingLen-k.now) <= binlog.SeqID {
-						k.ring[k.cfg.RingLen-int(k.batter+global.MaxSeqID-binlog.SeqID)] = bMsg
-						log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, k.cfg.RingLen-int(k.batter+global.MaxSeqID-binlog.SeqID))
-						break
-					} else {
-						time.Sleep(k.cfg.TimeSleep)
-					}
-
-				} else {
-					if k.batter-uint64(k.cfg.RingLen-k.now) > binlog.SeqID {
-						break
-					}
-					if k.batter-uint64(k.cfg.RingLen-k.now) < uint64(k.cfg.RingLen) && k.batter-uint64(10*k.cfg.RingLen-k.now)+global.MaxSeqID < binlog.SeqID {
-						break
-					}
-
-					if binlog.SeqID < k.batter+uint64(k.now) {
-						if k.batter > binlog.SeqID {
-							k.ring[k.cfg.RingLen-int(k.batter-binlog.SeqID)] = bMsg
-							log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, k.cfg.RingLen-int(k.batter-binlog.SeqID))
-						} else {
-							k.ring[binlog.SeqID-k.batter] = bMsg
-							log.Debugf("binlog msg: seqID:%d pos:%d", binlog.SeqID, binlog.SeqID-k.batter)
-						}
-						break
-					}
-					time.Sleep(k.cfg.TimeSleep)
-				}
-			}
+			k.ring.Set(bMsg)
 		}
 	}()
 	return pm, nil
@@ -239,6 +132,7 @@ func (k *KafkaConsumer) Close() {
 		return
 	}
 	k.closed = true
+	k.ring.Close()
 	for _, pc := range k.partitionConsumerArray {
 		_ = pc.consumer.Close()
 	}
---
+++ kafka/ringbuffer.go
@@ -0,0 +1,120 @@
+package kafka
+
+import (
+	"time"
+
+	"git.umlife.net/backend/mysql-bridge/global"
+	log "qiniupkg.com/x/log.v7"
+)
+
+type RingBuffer struct {
+	length int
+	sleep  time.Duration
+
+	buffer []*ConsumerMessage
+
+	now    int
+	seqID  uint64 //min seqID
+	batter uint64
+
+	channel chan *ConsumerMessage
+	closed  bool
+}
+
+func NewRingBuffer(length int, seqID uint64, sleep time.Duration) *RingBuffer {
+	if seqID < global.MinSeqID {
+		seqID = global.MinSeqID
+	}
+	r := &RingBuffer{
+		length:  length,
+		sleep:   sleep,
+		buffer:  make([]*ConsumerMessage, length),
+		now:     0,
+		seqID:   seqID,
+		batter:  seqID + uint64(length),
+		channel: make(chan *ConsumerMessage, 1024),
+	}
+	if r.batter > global.MaxSeqID {
+		r.batter = r.batter - global.MaxSeqID + global.MinSeqID - 1
+	}
+	go r.Run()
+	return r
+}
+
+func (r *RingBuffer) Run() {
+	for {
+		if r.closed {
+			return
+		}
+		if r.buffer[r.now] != nil {
+			if r.buffer[r.now].BinLog.SeqID == r.seqID {
+				log.Infof("get data index:%d expectSeqID:%d", r.now, r.seqID)
+				break
+			}
+			log.Errorf("ring bug! index:%d seqID:%d expectSeqID:%d", r.now,
+				r.buffer[r.now].BinLog.SeqID, r.seqID)
+		}
+		log.Infof("waiting for data... index:%d expectSeqID:%d", r.now, r.seqID)
+	}
+
+	r.channel <- r.buffer[r.now]
+	r.buffer[r.now] = nil
+
+	r.now++
+	if r.now == r.length {
+		r.now = 0
+		r.batter = r.batter + uint64(r.length)
+		if r.batter > global.MaxSeqID {
+			r.batter = r.batter - global.MaxSeqID + global.MinSeqID - 1
+		}
+	}
+
+	r.seqID++
+	if r.seqID > global.MaxSeqID {
+		r.seqID = global.MinSeqID
+	}
+	return
+}
+
+func (r *RingBuffer) Set(cm *ConsumerMessage) {
+	log.Infof("batter %d now %d binlog:%d ringLen:%d min: %d max: %d", r.batter, r.now, cm.BinLog.SeqID, r.length, global.MinSeqID, global.MaxSeqID)
+
+	for {
+		// 抛弃某些奇怪的ID
+		seqID := cm.BinLog.SeqID
+		if seqID < r.seqID && global.MaxSeqID-global.MinSeqID+1 > 2*(r.seqID-seqID) {
+			log.Warnf("duplicate %d seqID:%d min:%d max:%d", seqID, r.seqID, global.MinSeqID, global.MaxSeqID)
+			break
+		}
+
+		// 比较的时候方便
+		batter := r.batter
+		if batter <= r.seqID {
+			batter = batter + global.MaxSeqID - global.MinSeqID + 1
+		}
+		if seqID < r.seqID {
+			seqID = seqID + global.MaxSeqID - global.MinSeqID + 1
+		}
+
+		if seqID < batter {
+			r.buffer[r.length-int(batter-seqID)] = cm
+			log.Infof("binlog msg: seqID:%d pos:%d", cm.BinLog.SeqID, r.length-int(batter-seqID))
+			break
+		}
+		if seqID < r.seqID+uint64(r.length) {
+			r.buffer[seqID-batter] = cm
+			log.Infof("binlog msg: seqID:%d pos:%d", cm.BinLog.SeqID, seqID-batter)
+			break
+		}
+		time.Sleep(r.sleep)
+	}
+	return
+}
+
+func (r *RingBuffer) Close() {
+	if r.closed {
+		return
+	}
+	r.closed = true
+	return
+}
